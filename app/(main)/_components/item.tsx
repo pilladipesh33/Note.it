153727b5aed22c8263ea1db3edb71ca79dbeaee0
pilladipesh33 <pilladipesh.pd@gmail.com>
2023-12-05 19:07:00 +0530

    sidebar integrated

---
+++ app/(main)/_components/item.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import React from "react";
+
+type ItemProps = {
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
+  label: string;
+  onMouseClick: () => void;
+  icon: LucideIcon;
+};
+
+export const Item = ({
+  label,
+  onMouseClick,
+  icon: Icon,
+  id,
+  documentIcon,
+  active,
+  expanded,
+  onExpand,
+  isSearch,
+  level,
+}: ItemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
+  return (
+    <div
+      onClick={onMouseClick}
+      role="button"
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        active && "bg-primary/5 text-primary",
+      )}
+    >
+      {!!id && (
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+          onClick={() => {}}
+        >
+          <ChevronIcon />
+        </div>
+      )}
+      {documentIcon ? (
+        <div>{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
+      <span className="truncate">{label}</span>
+      {isSearch && (
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      )}
+    </div>
+  );
+};
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,14 +1,27 @@
 "use Client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import {
+  ChevronsLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from "./user-item";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
 
 export const Navigation = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
   const pathname = usePathname();
+  const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -92,6 +105,16 @@ export const Navigation = () => {
     }
   };
 
+  const handleCreateNote = () => {
+    const promise = create({ title: "New Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating new note...",
+      success: "Created a note",
+      error: "Failed to create new note",
+    });
+  };
+
   return (
     <div>
       <aside
@@ -113,10 +136,19 @@ export const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
+          <Item label="Search" icon={Search} isSearch onMouseClick={() => {}} />
+          <Item label="Settings" icon={Settings} onMouseClick={() => {}} />
+          <Item
+            onMouseClick={handleCreateNote}
+            label={"New page"}
+            icon={PlusCircle}
+          />
         </div>
         <div className="mt-4">
-          <p>Documents</p>
+          {documents?.map((document) => (
+            <p key={document?._id}>{document?.title}</p>
+          ))}
         </div>
         <div
           onMouseDown={handleMouseDown}
