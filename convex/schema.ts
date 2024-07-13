81990fb8d0d8e42959674ec497f72158552064f6
pilladipesh33 <pilladipesh.dp@gmail.com>
2024-07-13 22:14:28 +0530

    all error and warning fixed

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,7 +9,6 @@ export default defineSchema({
     parentDocument: v.optional(v.id("documents")),
     content: v.optional(v.string()),
     icon: v.optional(v.string()),
-    isPublished: v.boolean(),
   })
     .index("by_user", ["userId"])
     .index("by_user_parent", ["userId", "parentDocument"]),
